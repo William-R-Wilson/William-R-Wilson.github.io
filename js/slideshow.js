f96fc621379c6347bb28ea33cb79f5b772def78a
Will <williamrwilson@gmail.com>
2015-08-18 03:29:45 +0000

    add codepen to contact info and fix navbar

--- js/slideshow.js
+++ js/slideshow.js
@@ -6,7 +6,7 @@ var images = [
                 {source: "assets/images/reporting/dashboard.png", text: "Call up reports by period"},
                 {source: "assets/images/reporting/printable view.png", text: "Then print out the reports.  Printable view created with custom CSS"},
                 {source: "assets/images/reporting/responsive.png", text: "Custom responsive CSS ensures that users can report via mobile devices"},
-                {source: "assets/images/reporting/validations.png", text: "content is validated to prevent faulty data entry"},
+                {source: "assets/images/reporting/validations.png", text: "Content is validated to prevent faulty data entry"},
                 {source: "assets/images/reporting/validations2.png", text: ""}];
 
 
